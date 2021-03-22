c9310dbcb11fd8fead2874dc58ef9480ae8f44d5
pepiProg <74186271+pepiProg@users.noreply.github.com>
2021-03-22 15:40:31 +0200

    Add files via upload

--- library/Add/add.js
+++ library/Add/add.js
@@ -2,13 +2,13 @@ async function add(e) {
     e.preventDefault()
     const title = document.getElementById('title').value
     const author = document.getElementById('author').value
-    const description = document.getElementById('description').value
+    let description = document.getElementById('description').value
     const publisher = document.getElementById('publisher').value
     const yearOfPublishing = document.getElementById('year').value
     const img = document.getElementById('img').value
     const genre = document.getElementById('genre').value
     const book = { name: title, author, description, publisher, year: yearOfPublishing, img, genre }
-    if(title.trim()==''||author.trim()==''||description.trim()==''||publisher.trim()==''||yearOfPublishing.trim()==''||genre.trim()==''){
+    if(title.trim()==''||author.trim()==''||publisher.trim()==''||yearOfPublishing.trim()==''||genre.trim()==''){
         return alert('All book fields are required!')
     }
     fetch('https://books-76270-default-rtdb.firebaseio.com/books/.json', {
